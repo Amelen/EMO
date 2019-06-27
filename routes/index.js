b209271c5fc135fbae6ade9b9fb9d660eb525075
p0nch <petenka2003@gmail.com>
2019-06-27 12:39:48 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello, Peter!' });
+  res.render('index', { title: 'Hello, Kusya!' });
 });
 
 module.exports = router;
