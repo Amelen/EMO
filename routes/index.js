a4491e800e34fbe4983913abba9c49be9e6236bc
Amelen <41430690+Amelen@users.noreply.github.com>
2019-06-27 14:03:04 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello, Kusya!' });
+  res.render('index', { title: 'Hello, mmmmmmmrrrrrrrrrr!' });
 });
 
 module.exports = router;
